a5c5e351dc74f0d7c7f982c87eb94f910cdf3dfa
James Bremner <j.bremner@ymail.com>
2024-04-30 14:34:27 +0100

    add useEffect

--- react-gallery-app/src/App.jsx
+++ react-gallery-app/src/App.jsx
@@ -5,24 +5,28 @@ import Photo from './components/Photo';
 import Nav from './components/Nav';
 import Photolist from './components/PhotoList';
 import Search from './components/Search';
-
+import { useState, useEffect } from 'react';
 
 
 function App() {
-  const fetchData = async (searchText) => {
-    //fetch request
-    const response = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${searchText}&per_page=24&format=json&nojsoncallback=1`);
-    const data = await response.json();
-    console.log(data);
-    return data.photos.photo;
+  const [photoData, changeData] = useState(null);
 
-  }
+  useEffect(()=> {
+    const fetchData = async (queryText) => {
+      const response = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${queryText}&per_page=24&format=json&nojsoncallback=1`);
+      const data = await response.json();
+      changeData(data);
+    };
 
-  fetchData("sky");
+    fetchData("dog");
+  }, []);
+
+
   return (
     <>
      <Search />
      <Nav />
+     <Photolist photos={photoData}/>
      
     </>
   )
